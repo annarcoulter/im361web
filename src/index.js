4acfeff908f9a3356e9321a613fe52db23d1d7e3
Anna Coulter <arcoulter2@mail.bradley.edu>
2019-03-03 14:09:52 -0600

    almost donezo

--- src/index.js
+++ src/index.js
@@ -7,7 +7,26 @@ var app = new Vue({
   data: {
     pagename: 'Home',
     message: 'Hello Vue!',
-    counter:0
+    counter:0,
+    cart:[]
+  },
+  methods:{
+    addToCart: function (name, cost){
+      this.cart.push({
+        name:name,
+        cost:cost,
+      })
+    }
+  },
+  computed:{
+    cartTotal: function (){
+      var total=0;
+
+      for (var i= 0; i <this.cart.length; i++){
+        total += this.cart[i].cost;
+      }
+      return total;
+    }
   }
 });
 
